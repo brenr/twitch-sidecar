30a2a0d2b782cbd1ff0580220dc0136a208640db
Brendan Rosa <3654061+brenr@users.noreply.github.com>
2020-06-23 13:47:13 -0400

    Abstracted command loader (#1)
    * Working on abstracting out the command loader
    
    * Add codeowners
    
    * Fixed issue causing command loader to break when get method was invoked
    
    Co-authored-by: Brendan Rosa <bren@Brendans-iMac.local>

--- src/impl/type/PlayQueue.js
+++ src/impl/type/PlayQueue.js
@@ -1,29 +1,29 @@
-import Staff from "../Staff.js";
+import TwitchUser from "../TwitchUser.js";
 
 export default class PlayQueue {
 
     static #QUEUE = [];
     static #NOBODY_IN_QUEUE_MESSAGE = '/me There is nobody in the queue!';
 
-    static join(client, channel, username) {
-        if(PlayQueue.#QUEUE.indexOf(username) === -1) {
-            PlayQueue.#QUEUE.push(username);
-            client.say(channel, "/me @" + username + " has been added to the queue.");
+    static join(client, channel, context) {
+        if(PlayQueue.#QUEUE.indexOf(context.username) === -1) {
+            PlayQueue.#QUEUE.push(context.username);
+            client.say(channel, "/me @" + context.username + " has been added to the queue.");
         }
     }
 
-    static getNext(client, channel, username) {
-        if(Staff.isAdmin(username) || Staff.isMod(username)) {
+    static getNext(client, channel, context) {
+        if(TwitchUser.isBroadcaster(context) || TwitchUser.isModerator(context)) {
             if(PlayQueue.#QUEUE.length > 0) {
-                client.say(channel, "/me @" + PlayQueue.#QUEUE.shift(username) + " it's your turn to play!");
+                client.say(channel, "/me @" + PlayQueue.#QUEUE.shift(context.username) + " it's your turn to play!");
             } else {
                 client.say(channel, PlayQueue.#NOBODY_IN_QUEUE_MESSAGE);
             }
         }
     }
 
-    static getQueue(client, channel, username) {
-        if(Staff.isAdmin(username) || Staff.isMod(username)) {
+    static getQueue(client, channel, context) {
+        if(TwitchUser.isBroadcaster(context) || TwitchUser.isModerator(context)) {
             if(PlayQueue.#QUEUE.length > 0) {
                 client.say(channel, "/me Current queue: " + PlayQueue.#QUEUE.join(', '));
             } else {
